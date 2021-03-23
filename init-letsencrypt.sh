9d89bb0502c046ae335fbcd2bf0a3cc3c6ee2520
varveler <evarval@gmail.com>
2021-03-23 17:55:52 -0600

    added prod.yml

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,9 +7,9 @@ fi
 
 domains=(realedge.io)
 rsa_key_size=4096
-data_path="./certbot"
+data_path="./devops/docker/certbot"
 email="evarval@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
