82bd251e163be330c2fdd62f25256935cb244cd2
varveler <evarval@gmail.com>
2021-03-22 19:32:44 -0600

    secured settings

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -35,10 +35,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
