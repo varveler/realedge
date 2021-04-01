67d0428f06ee1e47822014b1810afe5b0b8f1a14
varveler <evarval@gmail.com>
2021-03-31 18:55:19 -0600

    changed urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('registers/', admin.site.urls),
     path('stocksearch/', include('stocksearch.urls'))
 ]
