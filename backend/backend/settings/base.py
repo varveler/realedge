67d734589f609ed9698c66bd1d1be8de6cd54267
varveler <evarval@gmail.com>
2021-05-07 09:10:56 -0500

    added bhb

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -166,9 +166,9 @@ if not BROKER_URL:
         hostname=RABBIT_HOSTNAME,
         vhost=os.environ.get('RABBIT_ENV_VHOST', ''))
 # We don't want to have dead connections stored on rabbitmq, so we have to negotiate using heartbeats
-# BROKER_HEARTBEAT = '?heartbeat=30'
+BROKER_HEARTBEAT = '?heartbeat=0'
 # https://stackoverflow.com/questions/14817181/django-celery-connectionerror-too-many-heartbeats-missed
-BROKER_HEARTBEAT = 0
+#BROKER_HEARTBEAT = 0
 if not BROKER_URL.endswith(BROKER_HEARTBEAT):
     BROKER_URL += BROKER_HEARTBEAT
 BROKER_POOL_LIMIT = 1
