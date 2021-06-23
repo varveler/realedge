3e34ccff78fd8e29cebaffe3a333b98b09cf8f55
varveler <evarval@gmail.com>
2021-06-23 13:28:38 -0500

    added links5

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -6,6 +6,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import store from './../store'
 import theme from './../theme';
 import styles from './../styles/material-kit-pro-react.css'
+import './../styles/empty.css'
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
