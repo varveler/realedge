22ea0412c1e880ce2a8a5e9a408d1a8863ef20a5
varveler <evarval@gmail.com>
2021-06-23 13:36:25 -0500

    added sign in link

--- frontend/src/features/navbar/NavBar.js
+++ frontend/src/features/navbar/NavBar.js
@@ -81,11 +81,18 @@ export default function NavBar() {
           </Tabs>
           </Grid>
           <Grid item>
-            {userIsLogedIn ?
+            {/*{userIsLogedIn ?
                             <Button component={ButtonLink} href={'/logout'} color="inherit">Log Out</Button>
                           : <Button component={ButtonLink} href={'/signin'} color="inherit">Sign In</Button>
+            } */}
+            {userIsLogedIn ?
+                            <Link href="/logout" passHref>
+                              <Tab component="a" label="logout" />
+                            </Link>
+                          : <Link href="/signin" passHref>
+                              <Tab component="a" label="signin" />
+                            </Link>
             }
-
           </Grid>
         </Grid>
         </Toolbar>
