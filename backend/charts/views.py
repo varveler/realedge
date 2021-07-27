1cd32731558275493b6ae2e0dac05494a2e797f2
varveler <evarval@gmail.com>
2021-07-27 17:38:50 -0500

    added commits

--- backend/charts/views.py
+++ backend/charts/views.py
@@ -15,8 +15,11 @@ from gappers.models import UpGapper
 @api_view(['GET', ])
 @authentication_classes((TokenAuthentication,))
 def chart_data(request, uuid):
+    print(uuid)
     if request.method == 'GET':
+        print(request)
         trade = Trade.objects.get(uuid=uuid)
+        print(trade)
         start, end = give_trade_chart_start_and_end_dates(trade)
         data = get_all_data('1Min', trade.ticker, start, end)
         data = apply_vwap_pandas(data)
@@ -34,12 +37,12 @@ def gapper_data(request, slug):
         date = slug.split('-')[1]
         gapper = UpGapper.objects.filter(ticker=ticker, date__year=date[0:4], date__month=date[4:6], date__day=date[6:8])
         if not gapper:
+            print('there is no gapper for', slug)
             return
         gapper = gapper[0]
         start, end = give_gapper_chart_start_and_end_dates(gapper)
         print(start, end, gapper.ticker)
         data = get_all_data('1Min', gapper.ticker, start, end)
-        print(data)
         data = apply_vwap_pandas(data)
         fix_data(data)
         return Response(data)
